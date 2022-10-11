e967aebf069be8990b07fb92037630a60b7051b2
Felipe Leite <lfelipe.leite86@gmail.com>
2022-10-10 21:08:05 -0300

    Basic Java Coding Challenges

---
+++ java-basic-challenges/src/ArraysPares.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class ArraysPares {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()); // Lê a linha de entrada
+
+        while (st.hasMoreTokens()) {
+            int value = Integer.parseInt(st.nextToken().replace(",", ""));
+            if (value%2 == 0)
+                System.out.println(value);
+        }
+    }
+}
---
+++ java-basic-challenges/src/PreenchimentoVetor.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PreenchimentoVetor {
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+        int t = leitor.nextInt();
+        int value = 0;
+
+        for (int i = 0; i < 1000; i++) {
+            if (value >= t)
+                value = 0;
+
+            System.out.println("N[" + i + "] = "+ value++);
+        }
+    }
+}
